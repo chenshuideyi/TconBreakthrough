d215d5e7387a7e719676a506d0eababb3a7d0717
CSDY <204125@qq.com>
2025-05-20 16:37:46 +0800

    把不必要的东西全删了

--- src/main/java/com/csdy/tconbreakthrough/ModMain.java
+++ src/main/java/com/csdy/tconbreakthrough/ModMain.java
@@ -1,10 +1,8 @@
 package com.csdy.tconbreakthrough;
 
 
-import com.csdy.tconbreakthrough.modifier.Register;
-import net.minecraftforge.eventbus.api.IEventBus;
+
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(ModMain.MODID)
 @Mod.EventBusSubscriber(modid = ModMain.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
@@ -13,8 +11,6 @@ public class ModMain {
     public static final String MODID = "tconbreakthrough";
 
     public ModMain() {
-        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-        Register.MODIFIERS.register(bus);
     }
 
 }
